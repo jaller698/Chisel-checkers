baf0783f7340fc9a1993b113f6a9df37754f694e
Leo Moe <leom@stud.ntnu.no>
2025-10-22 15:33:21 +0200

    bugfixes. legalmovesforwhite seems to work except for the boolean forcedmoves

--- src/main/scala/legalmovesforwhite.scala
+++ src/main/scala/legalmovesforwhite.scala
@@ -1,7 +1,12 @@
 import chisel3._
 import chisel3.util._
 
-
+/* 
+WE DONT ACTUALLY NEED THIS COMPONENT
+WE CAN JUST PUSH AND POP VALID MOVES. 
+WE USE THIS COMPONENT MAYBE ONCE IN THE END. 
+WE DO NEED TO FIGURE OUT FORCED MOVES BUT THAT IS MORE OR LESS IT!
+ */
 
 
 class LegalMovesForWhite() extends Module {
@@ -16,17 +21,18 @@ class LegalMovesForWhite() extends Module {
 
 
     private def idx(row: Int, col: Int): Int = {
-    if (row < 0 || row >= 8 || col < 0 || col >= 8) throw new IllegalArgumentException
-    if ((row + col) % 2 != 1) throw new IllegalArgumentException
+    if (row < 0 || row >= 8 || col < 0 || col >= 8) throw new IllegalArgumentException( s" $row  or $col out of range")
+    if ((row + col) % 2 != 1) throw new IllegalArgumentException( s" $row , $col hit a white square")
     return row * 4 + col / 2
   }
   val io = IO(new Bundle {
-    val In = Input(Wire(Vec(32, UInt (5.W))))
+    val In = Input(Vec(32, UInt (3.W)))
 
     val forcedMoves=Output(Bool())//THIS ONE ISN'T BEING SET AT ALL!
-    val whereWeCanMoveFrom=Output(Wire(Vec(32,Bool())))
+
+    val whereWeCanMoveFrom=Output(Vec(32,Bool()))
     //This one is if we can move from here. 
-    val whereWeCanMove=Output(Wire(Vec(4*32,Bool())))
+    val whereWeCanMove=Output(Vec(4*32,Bool()))
     /* 
     00: can go left
     01: can jump left
@@ -54,6 +60,16 @@ class LegalMovesForWhite() extends Module {
     val row_curr=row(i)
     val col_curr=col(i)
 
+    //The code to actually set forcedMoves: 
+    val jumps= new Array[chisel3.Bool](2*32)
+    var index:Int=0
+    for(i<-0 to 31){
+        if(i%4==1||i%4==3){
+            //TODO: DO AN ADD. 
+            index+=1
+        }
+    }
+
     //can move up left.
     if(row_curr>=1 && col_curr>=1){
         val to_1=idx(row_curr-1,col_curr-1)
@@ -69,8 +85,8 @@ class LegalMovesForWhite() extends Module {
         io.whereWeCanMove(i*4+1):=(
 
             io.In(i)==="b001".U&&
-            io.In(to_jump_over)==="b000".U&&
-            io.In(to_jump_to)==="b011".U//black normal piece. Can't jump over black king yet. 
+            io.In(to_jump_over)==="b011".U&&
+            io.In(to_jump_to)==="b000".U//black normal piece. Can't jump over black king yet. 
         )
 
     }
@@ -83,14 +99,13 @@ class LegalMovesForWhite() extends Module {
         //this one is white and the next one is empty
     }
     //can jump up over up right:
-    if(row_curr>=2&&col_curr>=7-2){
+    if(row_curr>=2&&col_curr<=7-2){
         val to_jump_over=idx(row_curr-1,col_curr+1)
         val to_jump_to=idx(row_curr-2,col_curr+2)
         io.whereWeCanMove(i*4+3):=(
-
             io.In(i)==="b001".U&&
-            io.In(to_jump_over)==="b000".U&&
-            io.In(to_jump_to)==="b011".U 
+            io.In(to_jump_over)==="b011".U&&
+            io.In(to_jump_to)==="b000".U 
         )
 
     }
