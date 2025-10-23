db54fa63b4d1b17d2ae02da198700068b5142bcc
LeoMoe <117756464+LeoMoeNTNU@users.noreply.github.com>
2025-10-23 11:01:53 +0200

    Legal moves for white (#10)
    * almost made legalmovesforwhitecomponent. No tests yet
    
    * bugfixes. legalmovesforwhite seems to work except for the boolean forcedmoves
    
    * Component implemented with tests. I may replace it with something smarter in the future
    
    * scala formatting
    
    ---------
    
    Co-authored-by: Leo Moe <leom@stud.ntnu.no>

---
+++ src/main/scala/legalmovesforwhite.scala
@@ -0,0 +1,150 @@
+import chisel3._
+import chisel3.util._
+
+/*
+WE DONT ACTUALLY NEED THIS COMPONENT
+WE CAN JUST PUSH AND POP VALID MOVES.
+WE USE THIS COMPONENT MAYBE ONCE IN THE END.
+WE DO NEED TO FIGURE OUT FORCED MOVES BUT THAT IS MORE OR LESS IT!
+ */
+
+class LegalMovesForWhite() extends Module {
+
+  // These are some filler functions I use.
+  private def row(i: Int) = i / 4
+  private def col(i: Int): Int = {
+    val r = row(i)
+    val offset = if (r % 2 == 0) 1 else 0
+    offset + 2 * (i % 4)
+  }
+  private def idx(row: Int, col: Int): Int = {
+    if (row < 0 || row >= 8 || col < 0 || col >= 8)
+      throw new IllegalArgumentException(s" $row  or $col out of range")
+    if ((row + col) % 2 != 1)
+      throw new IllegalArgumentException(s" $row , $col hit a white square")
+    return row * 4 + col / 2
+  }
+
+  val io = IO(new Bundle {
+    val In = Input(Vec(32, UInt(3.W)))
+
+    val forcedMoves = Output(Bool())
+
+    val whereWeCanMoveFrom = Output(Vec(32, Bool()))
+    // This one is if we can move from here.
+    val whereWeCanMove = Output(Vec(4 * 32, Bool()))
+    /*
+    00: can go left
+    01: can jump left
+    10: can go right
+    11: can jump right.
+     */
+
+  })
+
+  // io.forcedMoves:=false.B
+  io.whereWeCanMoveFrom := VecInit(Seq.tabulate(32) { i =>
+    false.B
+  })
+  io.whereWeCanMove := VecInit(Seq.tabulate(4 * 32) { i =>
+    false.B
+  })
+  for (i <- 0 to 31) {
+    io.whereWeCanMoveFrom(i) := (
+      io.whereWeCanMove(i * 4) || // can go up left
+        io.whereWeCanMove(i * 4 + 1) || // can jump over up left
+        io.whereWeCanMove(i * 4 + 2) || // can go up right
+        io.whereWeCanMove(i * 4 + 3) // can jump over up right
+    )
+    // Needs to take a lot into account.
+    val row_curr = row(i)
+    val col_curr = col(i)
+
+    // The code to actually set forcedMoves:
+    /*
+    val jumps= new Array[chisel3.Bool](2*32)
+
+    var index:Int=0
+    for(i<-0 to 31){
+        if(i%4==1||i%4==3){
+            //TODO: DO AN ADD.
+            index+=1
+        }
+    }
+     */
+    val canJumpArray = VecInit(io.whereWeCanMove.zipWithIndex.collect {
+      case (elem, ind) if (ind % 4 == 1 || ind % 4 == 3) => elem
+    })
+    io.forcedMoves := canJumpArray.reduceTree((x, y) => x || y)
+
+    // can move up left.
+    if (row_curr >= 1 && col_curr >= 1) {
+      val to_1 = idx(row_curr - 1, col_curr - 1)
+      // b001 is supposed to be white
+
+      io.whereWeCanMove(i * 4) := (io.In(i) === "b001".U && io.In(
+        to_1
+      ) === "b000".U)
+      // this one is white and the next one is empty
+    }
+    // can jump up left
+    if (row_curr >= 2 && col_curr >= 2) {
+      val to_jump_over = idx(row_curr - 1, col_curr - 1)
+      val to_jump_to = idx(row_curr - 2, col_curr - 2)
+      io.whereWeCanMove(i * 4 + 1) := (
+        io.In(i) === "b001".U &&
+          io.In(to_jump_over) === "b011".U &&
+          io.In(
+            to_jump_to
+          ) === "b000".U // black normal piece. Can't jump over black king yet.
+      )
+
+    }
+    // can move up right
+    if (row_curr >= 1 && col_curr <= 7 - 1) {
+      val to_1 = idx(row_curr - 1, col_curr + 1)
+      // b001 is supposed to be white
+
+      io.whereWeCanMove(i * 4 + 2) := io.In(i) === "b001".U && io.In(
+        to_1
+      ) === "b000".U
+      // this one is white and the next one is empty
+    }
+    // can jump up over up right:
+    if (row_curr >= 2 && col_curr <= 7 - 2) {
+      val to_jump_over = idx(row_curr - 1, col_curr + 1)
+      val to_jump_to = idx(row_curr - 2, col_curr + 2)
+      io.whereWeCanMove(i * 4 + 3) := (
+        io.In(i) === "b001".U &&
+          io.In(to_jump_over) === "b011".U &&
+          io.In(to_jump_to) === "b000".U
+      )
+
+    }
+
+    /*
+    if not all the way on the left
+        if can move up left:
+            wherewecanmovefrom(i):=1
+            wherewecanmove(i*4):=1
+
+    if not almost all on the left
+        if can jump up left:
+            wherewecanmovefrom(i):=1
+            wherewecanmove(i*4+1):=1
+
+    if not all on the right
+        if can move up right
+            wherewecanmovefrom(i):=1
+            wherewecanmove(i*4+2):=1
+
+    if not almost all on the right
+        if can jump up left
+            wherewecanmovefrom(i):=1
+            wherewecanmove(i*4+3):=1
+
+     */
+
+  }
+
+}
---
+++ src/test/scala/legalmovesforwhitetest.scala
@@ -0,0 +1,128 @@
+import chisel3._
+import chiseltest._
+import org.scalatest.flatspec.AnyFlatSpec
+
+class legalmovesforwhitetest extends AnyFlatSpec with ChiselScalatestTester {
+
+  behavior of "legalmovesforwhite_component"
+
+  it should "not have legal moves on empty board" in {
+    test(new LegalMovesForWhite()) { dut =>
+      for (i <- 0 to 31) {
+        dut.io.In(i).poke("b000".U)
+      }
+//        dut.io.In:=VecInit(Seq.tabulate(32) { i =>
+      //          "b000".U
+      //    })
+      dut.clock.step()
+      for (i <- 0 to 32 * 4 - 1) { // -1 because scala's range is shit.
+        dut.io
+          .whereWeCanMove(i)
+          .expect(
+            false.B,
+            s" $i should be 0, because the board is empty and thus no moves are possible!"
+          )
+      }
+      dut.io.forcedMoves.expect(false.B, s"there shouldnt be a forced move")
+
+    }
+  }
+
+  it should "input a few black pieces and have no legal moves" in {
+    test(new LegalMovesForWhite()) { dut =>
+      for (i <- 0 to 31) {
+        if (i == 5 || i == 7 || i == 17 | i == 22) {
+          dut.io.In(i).poke("b011".U)
+        } else {
+          dut.io.In(i).poke("b000".U)
+
+        }
+      }
+      dut.clock.step()
+      for (i <- 0 to 32 * 4 - 1) { // -1 because scala's range is shit.
+        dut.io
+          .whereWeCanMove(i)
+          .expect(
+            false.B,
+            s" $i should be 0, because the board is empty and thus no moves are possible!"
+          )
+      }
+      dut.io.forcedMoves.expect(false.B, s"there shouldnt be a forced move")
+    }
+  }
+
+  it should "place a piece in the middle of the board and see that it can move left and right" in {
+    test(new LegalMovesForWhite()) { dut =>
+      for (i <- 0 to 31) {
+        if (i == 14) {
+          dut.io.In(i).poke("b001".U)
+        } else {
+          dut.io.In(i).poke("b000".U)
+
+        }
+      }
+      dut.clock.step()
+      for (i <- 0 to 32 * 4 - 1) { // -1 because scala's range is shit.
+        if (i == 14 * 4 || i == 14 * 4 + 2) {
+          dut.io
+            .whereWeCanMove(i)
+            .expect(
+              true.B,
+              s" $i should be true, because the piece can jump from here"
+            )
+
+        } else {
+          dut.io
+            .whereWeCanMove(i)
+            .expect(
+              false.B,
+              s" $i should be 0, because the board is empty and thus no moves are possible!"
+            )
+
+        }
+      }
+      dut.io.forcedMoves.expect(false.B, s"there shouldnt be a forced move")
+    }
+  }
+
+  it should "place a white piece and black piece and be able to jump over" in {
+    test(new LegalMovesForWhite()) { dut =>
+      for (i <- 0 to 31) {
+
+        dut.io.In(i).poke("b000".U)
+      }
+
+      dut.io.In(26).poke("b001".U) // placing white
+      dut.io.In(23).poke("b011".U) // placing black
+      dut.clock.step()
+      for (i <- 0 to 32 * 4 - 1) { // -1 because scala's range is shit.
+        if (i == 26 * 4 + 3) {
+          dut.io
+            .whereWeCanMove(i)
+            .expect(
+              true.B,
+              s" $i should be true, because the piece can jump from here"
+            )
+
+        } else if (i == 26 * 4) {
+          dut.io
+            .whereWeCanMove(i)
+            .expect(
+              true.B,
+              s" $i should be true, because the piece can go to the left from here"
+            )
+        } else {
+          dut.io
+            .whereWeCanMove(i)
+            .expect(
+              false.B,
+              s" $i should be 0, because the board is empty and thus no moves are possible!"
+            )
+
+        }
+      }
+      dut.io.forcedMoves.expect(true.B, s"the move should be forced")
+    }
+  }
+
+}
