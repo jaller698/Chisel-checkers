9a003a89bb5d04326a7833bb9ed252aa235dddb2
askemoprasmussen <161342255+askemoprasmussen@users.noreply.github.com>
2025-11-04 16:24:22 +0100

    Removed debug io pins

--- src/test/scala/IntegrationTests.scala
+++ src/test/scala/IntegrationTests.scala
@@ -19,7 +19,6 @@ class IntegrationTests extends AnyFlatSpec with ChiselScalatestTester {
       for (i <- 0 to 31) {
         dut.io.from.poke(i.U)
         dut.clock.step()
-        dut.io.colorAtTile.expect(0.U, s" $i should be 0, which is empty")
       }
     }
   }
@@ -70,18 +69,12 @@ class IntegrationTests extends AnyFlatSpec with ChiselScalatestTester {
 
       dut.io.from.poke(20.U)
       dut.clock.step()
-      dut.io.colorAtTile
-        .expect(1.U, "Tile 20 should still have white piece")
 
       dut.io.from.poke(10.U)
       dut.clock.step()
-      dut.io.colorAtTile
-        .expect(3.U, "Tile 10 should still have black piece")
 
       dut.io.from.poke(16.U)
       dut.clock.step()
-      dut.io.colorAtTile
-        .expect(0.U, "Tile 16 should still be empty")
 
     }
   }
@@ -110,9 +103,6 @@ class IntegrationTests extends AnyFlatSpec with ChiselScalatestTester {
 
       dut.io.from.poke(20.U)
       dut.clock.step()
-      dut.io.colorAtTile
-        .expect(1.U, "Board remained the same through mode switches")
-
     }
   }
 
@@ -131,8 +121,6 @@ class IntegrationTests extends AnyFlatSpec with ChiselScalatestTester {
       dut.io.mode.poke("b10".U)
       dut.io.from.poke(15.U)
       dut.clock.step()
-      dut.io.colorAtTile
-        .expect(1.U, "Tile 15 should have white piece")
 
       dut.io.mode.poke("b00".U)
       dut.io.reset.poke(true.B)
@@ -143,20 +131,14 @@ class IntegrationTests extends AnyFlatSpec with ChiselScalatestTester {
       for (i <- 0 to 11) {
         dut.io.from.poke(i.U)
         dut.clock.step()
-        dut.io.colorAtTile
-          .expect(3.U, s"Tile $i should be black")
       }
       for (i <- 12 to 19) {
         dut.io.from.poke(i.U)
         dut.clock.step()
-        dut.io.colorAtTile
-          .expect(0.U, s"Tile $i should be empty")
       }
       for (i <- 20 to 31) {
         dut.io.from.poke(i.U)
         dut.clock.step()
-        dut.io.colorAtTile
-          .expect(1.U, s"Tile $i should be white")
       }
     }
   }
@@ -176,8 +158,6 @@ class IntegrationTests extends AnyFlatSpec with ChiselScalatestTester {
       dut.io.mode.poke("b10".U)
       dut.io.from.poke(20.U)
       dut.clock.step()
-      dut.io.colorAtTile
-        .expect(1.U, "Tile 20 should have white piece")
 
       dut.io.mode.poke("b00".U)
       dut.io.reset.poke(true.B)
@@ -187,8 +167,6 @@ class IntegrationTests extends AnyFlatSpec with ChiselScalatestTester {
       dut.io.mode.poke("b10".U)
       dut.io.from.poke(20.U)
       dut.clock.step()
-      dut.io.colorAtTile
-        .expect(0.U, "Piece at position 20 should be empty")
 
       dut.io.mode.poke("b00".U)
       dut.io.reset.poke(true.B)
@@ -199,20 +177,14 @@ class IntegrationTests extends AnyFlatSpec with ChiselScalatestTester {
       for (i <- 0 to 11) {
         dut.io.from.poke(i.U)
         dut.clock.step()
-        dut.io.colorAtTile
-          .expect(3.U, s"Tile $i should be black")
       }
       for (i <- 12 to 19) {
         dut.io.from.poke(i.U)
         dut.clock.step()
-        dut.io.colorAtTile
-          .expect(0.U, s"Tile $i should be empty")
       }
       for (i <- 20 to 31) {
         dut.io.from.poke(i.U)
         dut.clock.step()
-        dut.io.colorAtTile
-          .expect(1.U, s"Tile $i should be white")
       }
     }
   }
