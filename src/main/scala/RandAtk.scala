71c631503ecc742f28974b81b1b92f20731b10d8
NA33AT0R <Gabrielllarsen@outlook.com>
2025-11-11 15:56:02 +0100

    Now picks random attack

---
+++ src/main/scala/RandAtk.scala
@@ -0,0 +1,59 @@
+import chisel3._
+import chisel3.util._
+
+class RandomAttack extends Module {
+
+  private def row(i: Int) = i / 4
+  private def col(i: Int): Int = {
+    val r = row(i)
+    val offset = if (r % 2 == 0) 1 else 0
+    offset + 2 * (i % 4)
+  }
+  private def idx(row: Int, col: Int): Int = {
+    if (row < 0 || row >= 8 || col < 0 || col >= 8)
+      throw new IllegalArgumentException(s" $row  or $col out of range")
+    if ((row + col) % 2 != 1)
+      throw new IllegalArgumentException(s" $row , $col hit a white square")
+    return row * 4 + col / 2
+  }
+
+  val io = IO(new Bundle {
+    val board = Input(Vec(32, UInt(3.W)))
+    val whereWeCanMove = Input(Vec(4 * 32, Bool()))
+    val boardWrite = Output(Vec(32, UInt(3.W)))
+  })
+
+  val sEmpty :: sWhite :: sWhiteKing :: sBlack :: sBlackKing :: Nil = Enum(5)
+  io.boardWrite := io.board
+
+  for (k <- 1 to 127 by 2) {
+
+    when(io.whereWeCanMove(k) === true.B) {
+      val piece = (k / 4)
+      val row_curr = row(piece)
+      val col_curr = col(piece)
+
+      if (k % 4 == 1) {
+        if (row_curr - 2 >= 0 && col_curr - 2 >= 0) {
+          val to_jump_over = idx(row_curr - 1, col_curr - 1)
+          val to_jump_to = idx(row_curr - 2, col_curr - 2)
+          io.boardWrite(piece) := sEmpty
+          io.boardWrite(to_jump_over) := sEmpty
+          io.boardWrite(to_jump_to) := sWhite
+        }
+      }
+      if (k % 4 == 3) {
+        if (row_curr - 2 >= 0 && col_curr + 2 <= 7) {
+          val to_jump_over = idx(row_curr - 1, col_curr + 1)
+          val to_jump_to = idx(row_curr - 2, col_curr + 2)
+          io.boardWrite(piece) := sEmpty
+          io.boardWrite(to_jump_over) := sEmpty
+          io.boardWrite(to_jump_to) := sWhite
+        }
+      }
+
+    }
+
+  }
+
+}
---
+++ src/test/scala/RandAtkTest.scala
@@ -0,0 +1,41 @@
+import chisel3._
+import chisel3.util._
+import chiseltest._
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+
+class RandAtkTest extends AnyFlatSpec with ChiselScalatestTester with Matchers  {
+
+  behavior of "RandAtk"
+
+  it should "have a forcedmove with a black pawn on 22 and a white pawn on 26" in {
+    test(new RandomAttack()) { dut =>
+      val sEmpty :: sWhite :: sWhiteKing :: sBlack :: sBlackKing :: Nil =
+        Enum(5)
+
+      val testVectors = Seq.tabulate(32) { i =>
+        if (i < 12) sBlack
+        else if (i == 16) sBlack
+        else if (i >= 20) sWhite
+        else sEmpty
+      }
+      val testAtks = Seq.tabulate(128) { i =>
+        if (i == 83) true.B
+        else false.B
+      }
+      for (i <- 0 to 31) {
+        dut.io.board(i).poke(testVectors(i))
+      }
+      for (i <- 0 to 127) {
+        dut.io.whereWeCanMove(i).poke(testAtks(i))
+      }
+
+      dut.clock.step()
+      dut.io.boardWrite(20).expect(sEmpty)
+      dut.io.boardWrite(16).expect(sEmpty)
+      dut.io.boardWrite(13).expect(sWhite)
+    }
+  }
+
+}
