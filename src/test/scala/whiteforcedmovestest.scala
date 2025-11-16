cddf06e10dcab6427173417433e8cfc316309945
askemoprasmussen <161342255+askemoprasmussen@users.noreply.github.com>
2025-11-16 14:26:01 +0100

    Create whiteforcedmovestest.scala

---
+++ src/test/scala/whiteforcedmovestest.scala
@@ -0,0 +1,42 @@
+import chisel3._
+import chiseltest._
+import org.scalatest.flatspec.AnyFlatSpec
+
+class whiteforcedmovestest extends AnyFlatSpec with ChiselScalatestTester {
+
+  behavior of "whiteforcedmoves"
+
+  it should "not have forced moves on an empty board" in {
+    test(new WhiteForcedMoves()) { dut =>
+      for (i <- 0 to 31) {
+        dut.io.In(i).poke("b000".U)
+      }
+
+      dut.io.out.expect(
+        false.B,
+        "there shouldn't be forced moves on an empty board"
+      )
+    }
+  }
+
+  it should "have a forced move with a white pawn on 26 and a black pawn on 23" in {
+    test(new WhiteForcedMoves()) { dut =>
+      for (i <- 0 to 31) {
+        if (i == 26) {
+          dut.io.In(i).poke("b001".U) // white pawn
+        } else if (i == 23) {
+          dut.io.In(i).poke("b011".U) // black pawn to jump over
+        } else {
+          dut.io.In(i).poke("b000".U) // empty
+        }
+      }
+
+      dut.io.out.expect(
+        true.B,
+        "should be a forced move here!"
+      )
+    }
+  }
+}
+
+
