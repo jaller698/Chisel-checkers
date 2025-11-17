0ebee872e41761cd1d93f356587651dd5915ec14
NA33AT0R <Gabrielllarsen@outlook.com>
2025-11-17 09:39:31 +0100

    ad

--- src/test/scala/RandAtkTest.scala
+++ src/test/scala/RandAtkTest.scala
@@ -9,7 +9,7 @@ class RandAtkTest extends AnyFlatSpec with ChiselScalatestTester with Matchers  
 
   behavior of "RandAtk"
 
-  it should "have a forcedmove with a black pawn on 22 and a white pawn on 26" in {
+  it should "RandAtk Leads to 21 taking 16 moving to 12" in {
     test(new RandomAttack()) { dut =>
       val sEmpty :: sWhite :: sWhiteKing :: sBlack :: sBlackKing :: Nil =
         Enum(5)
@@ -41,6 +41,52 @@ class RandAtkTest extends AnyFlatSpec with ChiselScalatestTester with Matchers  
       dut.io.boardWrite(21).expect(sEmpty)
       dut.io.boardWrite(16).expect(sEmpty)
       dut.io.boardWrite(12).expect(sWhite)
+    }
+  }
+  it should "Random Movement" in {
+    test(new RandomAttack()) { dut =>
+      val sEmpty :: sWhite :: sWhiteKing :: sBlack :: sBlackKing :: Nil =
+        Enum(5)
+
+      val testVectors = Seq.tabulate(32) { i =>
+        if (i < 12) sBlack
+        else if (i == 16) sBlack
+        else if (i >= 20) sWhite
+        else sEmpty
+      }
+
+      //space*4 + 0/1/2/3 (0, is move up left, 1 atk up left, 2 move up right, 3 atk up right)
+      val testAtks = Seq.tabulate(128) { i =>
+        if (i == 82) true.B 
+        else if (i == 84) true.B 
+        else if (i == 86) true.B
+        else if (i == 88) true.B
+        else if (i == 90) true.B
+        else if (i == 92) true.B
+        else if (i == 94) true.B
+        else false.B
+      }
+      dut.io.AtkPresent.poke(false.B)
+
+      for (i <- 0 to 31) {
+        dut.io.board(i).poke(testVectors(i))
+      }
+      for (i <- 0 to 127) {
+        dut.io.whereWeCanMove(i).poke(testAtks(i))
+      }
+
+      dut.clock.step()
+
+      dut.io.boardWrite(23).expect(sEmpty)
+      dut.io.boardWrite(18).expect(sWhite)
+      dut.io.boardWrite(21).expect(sWhite)
+      dut.io.boardWrite(22).expect(sWhite)
+      dut.io.boardWrite(20).expect(sWhite)
+      
+
+
+
+
     }
   }
 
